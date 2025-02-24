871a227c48df95e48a809e8fd66ffea964f0b7fd
rjheby <69710326+rjheby@users.noreply.github.com>
2025-02-23 22:20:02 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { UserProvider } from './context/UserContext' // Import UserProvider
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <UserProvider> {/* Wrap the app in UserProvider */}
+      <App />
+    </UserProvider>
   </BrowserRouter>
 );
