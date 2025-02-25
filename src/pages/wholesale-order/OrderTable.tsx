02c952a825eb1b38b09be1822d9c65a346a804e0
rjheby <69710326+rjheby@users.noreply.github.com>
2025-02-25 00:27:51 -0500

    Update OrderTable.tsx

--- src/pages/wholesale-order/OrderTable.tsx
+++ src/pages/wholesale-order/OrderTable.tsx
@@ -26,50 +26,46 @@ export function OrderTable() {
   } = useOrderTable();
 
   return (
-    <div className="overflow-x-auto">
-      <div className="hidden md:block">
-        <div className="w-full inline-block align-middle">
-          <div className="overflow-hidden">
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead className="w-1/6">Name</TableHead>
-                  {optionFields.map((field) => (
-                    <TableHead key={field} className="w-1/12">
-                      {field.charAt(0).toUpperCase() + field.slice(1)}
-                    </TableHead>
-                  ))}
-                  <TableHead className="w-1/12">Quantity</TableHead>
-                  <TableHead className="w-1/12">Unit Cost</TableHead>
-                  <TableHead className="w-1/12">Total Cost</TableHead>
-                  <TableHead className="w-1/10">Actions</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {items.map((item) => (
-                  <OrderTableRow
-                    key={item.id}
-                    item={item}
-                    options={options}
-                    isAdmin={isAdmin}
-                    editingField={editingField}
-                    newOption={newOption}
-                    onNewOptionChange={setNewOption}
-                    onKeyPress={handleKeyPress}
-                    onUpdateItem={handleUpdateItem}
-                    onRemoveRow={handleRemoveRow}
-                    onCopyRow={handleCopyRow}
-                    onAddItem={handleAddItem}
-                    generateItemName={generateItemName}
-                    onUpdateOptions={handleUpdateOptions}
-                    isCompressed={!!compressedStates[item.id]}
-                    onToggleCompressed={toggleCompressed}
-                  />
-                ))}
-              </TableBody>
-            </Table>
-          </div>
-        </div>
+    <div className="overflow-x-auto -mx-2 sm:-mx-4 md:mx-0">
+      <div className="hidden md:block min-w-full">
+        <Table className="w-full table-fixed">
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-1/6">Name</TableHead>
+              {optionFields.map((field) => (
+                <TableHead key={field} className="w-1/12 truncate">
+                  {field.charAt(0).toUpperCase() + field.slice(1)}
+                </TableHead>
+              ))}
+              <TableHead className="w-1/12">Qty</TableHead>
+              <TableHead className="w-1/12">Unit</TableHead>
+              <TableHead className="w-1/12">Total</TableHead>
+              <TableHead className="w-1/12">Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {items.map((item) => (
+              <OrderTableRow
+                key={item.id}
+                item={item}
+                options={options}
+                isAdmin={isAdmin}
+                editingField={editingField}
+                newOption={newOption}
+                onNewOptionChange={setNewOption}
+                onKeyPress={handleKeyPress}
+                onUpdateItem={handleUpdateItem}
+                onRemoveRow={handleRemoveRow}
+                onCopyRow={handleCopyRow}
+                onAddItem={handleAddItem}
+                generateItemName={generateItemName}
+                onUpdateOptions={handleUpdateOptions}
+                isCompressed={!!compressedStates[item.id]}
+                onToggleCompressed={toggleCompressed}
+              />
+            ))}
+          </TableBody>
+        </Table>
       </div>
 
       <div className="md:hidden">
