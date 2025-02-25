b9cd679abd578c37a98329b6161724db0073b9ed
rjheby <69710326+rjheby@users.noreply.github.com>
2025-02-25 00:09:53 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-
 import { Routes, Route, Navigate } from "react-router-dom";
 import { WholesaleOrder } from "./pages/WholesaleOrder";
 import { WholesaleOrderForm } from "./pages/WholesaleOrderForm";
 import { Toaster } from "@/components/ui/toaster";
 import AppLayout from "@/components/layouts/AppLayout";
 import { AdminProvider } from "@/context/AdminContext";
+import DispatchDelivery from "./pages/DispatchDelivery";
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
           <Route path="/" element={<Navigate to="/wholesale-order" replace />} />
           <Route path="/wholesale-order" element={<WholesaleOrder />} />
           <Route path="/wholesale-order-form/:id" element={<WholesaleOrderForm />} />
+          <Route path="/dispatch-delivery" element={<DispatchDelivery />} />
         </Routes>
       </AppLayout>
       <Toaster />
